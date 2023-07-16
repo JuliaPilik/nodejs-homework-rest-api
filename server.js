d1a28571173b89bcd82d3bc2426e5ed4aedbfca6
Julia Meshchaninova <j.meschaninova@gmail.com>
2023-07-16 17:58:35 +0300

    add evn.example

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT=3000 } = process.env;
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   })
